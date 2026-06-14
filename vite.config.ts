bdb1d9a07b79ae073f710849acae0b2f2caaf0bd
VaJoy Lan <vajoy@qq.com>
2026-06-14 15:23:56 +0800

    chore: update

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite-plus";
-
-export default defineConfig({
-  /** 配置 Staged 规则 **/
-  staged: {
-    'vue/*.{js,json}': ['vp fmt --no-error-on-unmatched-pattern'],
-    'vue/*.ts?(x)': ['vp lint --fix', 'vp fmt --no-error-on-unmatched-pattern'],
-  }
-})
